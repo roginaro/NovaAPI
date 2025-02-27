f20d06e83b9c45291cbc57e894a85ea4d41d0076
roginaro <rogicavalcante@gmail.com>
2025-02-27 13:02:08 +0000

    First Commit

---
+++ src/NovaAPI.api/Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NovaAPI.repositories.Interfaces;
+using NovaAPI.repositories.Repositories;
+
+namespace NovaAPI.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProduto _produto;
+
+        public ProdutoController(IProduto produto)
+        {
+            _produto = produto;
+        }
+
+        [HttpGet(Name = "produto")]
+        public IEnumerable<string> Get()
+        {
+            return _produto.GetAll();
+        }
+
+    }
+}
---
+++ src/NovaAPI.repositories/Interfaces/IProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaAPI.repositories.Interfaces
+{
+    public interface IProduto
+    {
+        List<string> GetAll();
+    }
+}
