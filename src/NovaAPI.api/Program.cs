4e246e4bc6e7a9dac93b6c602df882215cf16ef0
Jairo Azevedo <jsouza.lp@gmail.com>
2025-02-27 10:29:18 -0300

    Review
    Review inicial de alguns pontos

--- src/NovaAPI.api/Program.cs
+++ src/NovaAPI.api/Program.cs
@@ -9,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Para a program.cs, recomendo criar uma extension só para a injeção de dependência e por camada
+// Por exemplo, quando você tiver a sua camada de services, você pode criar uma extension só para ela fazer as injeções 
+// Lembre: A injeção de dependência é feita por camada, então você pode ter uma extension para cada camada
+// Lembre 2: As extensions podem ser criadas em um projeto separado, por exemplo, NovaAPI.infra mas não é aconselhável pois criará um acoplamento entre os projetos
+// Lembre 3: Normalmente criamos extensios por assunto, por exemplo, uma extension para injeção de dependência, outra para configuração de swagger, outra para configuração de banco de dados, etc.
 builder.Services.AddScoped<IProduto, ProdutoRepository>();
 
 
--- src/NovaAPI.repositories/Interfaces/IProduto.cs
+++ src/NovaAPI.repositories/Interfaces/IProduto.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace NovaAPI.repositories.Interfaces
 {
+    // Procure sempre dar nomes mais intuitivos para as interfaces.
+    // Por exemplo, se você está na camada de repositories, poderia ser IProdutoRepository.
+    // Uma outra sugestão: Utilize sempre nomes em inglês, por exemplo IProductRepository.
+    // Por que disseo? Porque se você for trabalhar em um projeto com pessoas de outros países, você já estará acostumado
+    // Um outro ponto é que vc declarou IProduto (PT) e o nome do método é GetAll, então seria melhor IProductRepository e GetAll OU IProduto e ObterTodos.
     public interface IProduto
     {
         List<string> GetAll();
