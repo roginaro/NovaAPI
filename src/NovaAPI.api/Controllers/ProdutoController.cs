53f26dcfaae7bd4509fdf1a753bec502704c62b5
roginaro <rogicavalcante@gmail.com>
2025-02-28 18:07:44 +0000

    Atualização com os cometarios

--- src/NovaAPI.api/Controllers/ProdutoController.cs
+++ src/NovaAPI.api/Controllers/ProdutoController.cs
@@ -16,9 +16,13 @@ namespace NovaAPI.api.Controllers
             _produto = produto;
         }
 
+        // Sugestão: Se é um get da controller produtos, não precisaria de um nome para a rota.
+        // A api ficará mais limpa e intuitiva.
         [HttpGet(Name = "produto")]
         public IEnumerable<string> Get()
         {
+            // Num caso real, não é aconselhável a API atacar diretamente o banco de dados.
+            // API -> Services -> Repositories -> Banco de Dados
             return _produto.GetAll();
         }
 
